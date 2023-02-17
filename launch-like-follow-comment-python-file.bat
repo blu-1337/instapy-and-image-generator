ba82e3cd785f7875e18ccafe25564060f4e6e731
blu <n1ilmpg@gmail.com>
2023-02-17 18:56:00 +0200

    makes small changes for running python script before getting account suspended

--- launch-like-follow-comment-python-file.bat
+++ launch-like-follow-comment-python-file.bat
@@ -1,5 +1,4 @@
 @echo on
 E:
 cd projects\instapy-and-image-generator\
-"C:\Program Files\Python38\python.exe" "like-follow-comment.py"
-pause
+"C:\Program Files\Python38\python.exe" like-follow-comment.py
